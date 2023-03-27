65f6b0e0d77d7d163e154dfb591f6e0791a2afe5
rostianna <rostiannasinuraya1@gmail.com>
2023-03-27 16:53:11 +0700

    first commit

---
+++ Proses.php
@@ -0,0 +1,21 @@
+
+<?php
+$no1 = $_POST['txtbil1'];
+$no2 = $_POST['txtbil2'];
+echo "Bilangan 1 : $no1 <br>";
+echo "Bilangan 2 : $no2 <br>";
+$tambah = $no1 + $no2;
+echo "Hasil Tambah = $tambah <br>";
+
+$kurang = $no1 - $no2;
+echo "Hasil Kurang = $kurang <br>";
+
+$kali = $no1 * $no2;
+echo "Hasil Kali = $kali <br>";
+
+
+$bagi = $no1 / $no2;
+echo "Hasil Bagi = $bagi <br>";
+?>
+
+
